69e387fe01b479cf181636ee6de260e015be2ef2
Ankoncha <44019225+luciferankon@users.noreply.github.com>
2019-07-09 00:58:13 +0530

    Slack notifier for sauron

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/nlopes/slack"
+)
+
+func main() {
+	if len(os.Args) < 4 {
+		log.Fatal("invalid arguments")
+	}
+	userName := os.Args[2]
+	message := os.Args[3]
+
+	api := slack.New(os.Args[1])
+	users, _ := api.GetUsers()
+	var userId string
+	for _, user := range users {
+		if user.Name == userName {
+			userId = user.ID
+		}
+	}
+	_, _, channelID, _ := api.OpenIMChannel(userId)
+	m := slack.MsgOptionText(message, true)
+	api.SendMessage(channelID, m)
+}
